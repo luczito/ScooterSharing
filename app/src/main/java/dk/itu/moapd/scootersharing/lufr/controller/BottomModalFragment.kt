ba49e220b5776086ccfdc62bd178d6418a0d9eaf
Rasmus Nielsen <rasni@itu.dk>
2023-04-29 17:46:09 +0200

    Implement update scooter image feature

--- app/src/main/java/dk/itu/moapd/scootersharing/lufr/controller/BottomModalFragment.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/lufr/controller/BottomModalFragment.kt
@@ -3,6 +3,8 @@ package dk.itu.moapd.scootersharing.lufr.controller
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -13,6 +15,7 @@ import android.location.Geocoder
 import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.provider.MediaStore
 import android.util.DisplayMetrics
@@ -22,8 +25,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -31,26 +38,42 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 import dk.itu.moapd.scootersharing.lufr.R
 import dk.itu.moapd.scootersharing.lufr.controller.SignupFragment.Companion.TAG
 import dk.itu.moapd.scootersharing.lufr.databinding.FragmentBottomModalBinding
 import dk.itu.moapd.scootersharing.lufr.model.RidesDB
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import java.sql.Date
 import java.text.SimpleDateFormat
+import java.util.Locale
+import kotlin.math.roundToInt
 
 
 class BottomModalFragment(private val marker: Marker) : BottomSheetDialogFragment() {
 
     companion object {
         private const val REQUEST_IMAGE_CAPTURE = 1
+        private const val REQUEST_CAMERA_PERMISSION = 2
     }
 
+
     private lateinit var binding: FragmentBottomModalBinding
+
+    private lateinit var takePictureLauncher: ActivityResultLauncher<Intent>
+    private lateinit var imageBitmap: Bitmap
+
+    private var takePhoto: ActivityResultLauncher<Uri>? = null
+
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var auth: FirebaseAuth
@@ -62,8 +85,27 @@ class BottomModalFragment(private val marker: Marker) : BottomSheetDialogFragmen
     private var location: String? = null
     private var timestamp: String? = null
     private var reserved: String? = null
+
+    private var photoName: String? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        takePictureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//            if (result.resultCode == Activity.RESULT_OK) {
+//                // The picture was taken successfully, handle the result here
+//                val data: Intent? = result.data
+//                imageBitmap = data?.extras?.get("data") as Bitmap
+//                uploadImageToFirebase(imageBitmap)
+//            } else {
+//                // The picture taking was cancelled by the user
+//                Log.d(TAG, "Picture taking cancelled")
+//            }
+//        }
+
+
+
         arguments?.let {
             name = it.getString("name")
             location = it.getString("location")
@@ -75,6 +117,20 @@ class BottomModalFragment(private val marker: Marker) : BottomSheetDialogFragmen
         }
         auth = Firebase.auth
 
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        takePhoto = registerForActivityResult(ActivityResultContracts.TakePicture()) { didTakePhoto: Boolean ->
+            if (didTakePhoto && photoName != null) {
+                openCamera()
+            }
+        }
+    }
+    override fun onDetach() {
+        super.onDetach()
+        takePhoto = null
     }
 
     override fun onCreateView(
@@ -168,6 +224,29 @@ class BottomModalFragment(private val marker: Marker) : BottomSheetDialogFragmen
         }
 
         binding.apply {
+
+
+                pictureButton.setOnClickListener {
+                    if (ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.CAMERA
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        requestCameraPermission()
+                    } else {
+                    photoName = "IMG_${name}.JPG"
+                    val photoFile = File(requireContext().applicationContext.filesDir, photoName)
+                    photoName = photoFile.absolutePath
+                    val photoUri = FileProvider.getUriForFile(
+                        requireContext(),
+                        "dk.itu.moapd.scootersharing.lufr.fileprovider",
+                        photoFile
+                    )
+                    takePhoto?.launch(photoUri)
+            }
+            }
+
+
             reserveButton.setOnClickListener {
                 if (reserved == auth.currentUser?.email.toString()) {
                     changeColor("blue")
@@ -227,14 +306,17 @@ class BottomModalFragment(private val marker: Marker) : BottomSheetDialogFragmen
                         }
                         .setPositiveButton("End") { _, _ ->
 
+
                             getLocation {lat, long ->
-                                val geoCoder = Geocoder(requireContext())
-                                val matches = geoCoder.getFromLocation(lat, long, 1)
-                                val bestMatch = if (matches!!.isEmpty()) null else matches[0]
-                                RidesDB.updateScooter(name!!, bestMatch!!.getAddressLine(0), System.currentTimeMillis(), lat, long)
-                                marker.position = LatLng(lat, long)
-                                marker.isVisible = false
-                                marker.isVisible = true
+                                if(isAdded) {
+                                    val geoCoder = Geocoder(requireContext())
+                                    val matches = geoCoder.getFromLocation(lat, long, 1)
+                                    val bestMatch = if (matches!!.isEmpty()) null else matches[0]
+                                    RidesDB.updateScooter(name!!, bestMatch!!.getAddressLine(0), System.currentTimeMillis(), lat, long)
+                                    marker.position = LatLng(lat, long)
+                                    marker.isVisible = false
+                                    marker.isVisible = true
+                                }
                             }
 
 
@@ -267,6 +349,7 @@ class BottomModalFragment(private val marker: Marker) : BottomSheetDialogFragmen
                             ).show()
                         }
                         .show()
+
                 }
             }
         }
@@ -283,6 +366,117 @@ class BottomModalFragment(private val marker: Marker) : BottomSheetDialogFragmen
 
         handler.postDelayed(updateTextViewRunnable, 1000)
     }
+
+    fun getScaledBitmap(path: String, destWidth: Int, destHeight: Int): Bitmap {
+        // Read in the dimensions of the image on disk
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(path, options)
+        val srcWidth = options.outWidth.toFloat()
+        val srcHeight = options.outHeight.toFloat()
+        // Figure out how much to scale down by
+        val sampleSize = if (srcHeight <= destHeight && srcWidth <= destWidth) {
+            1
+        } else {
+            val heightScale = srcHeight / destHeight
+            val widthScale = srcWidth / destWidth
+            minOf(heightScale, widthScale).roundToInt()
+        }
+        // Read in and create final bitmap
+        return BitmapFactory.decodeFile(path, BitmapFactory.Options().apply {
+            inSampleSize = sampleSize
+        })
+    }
+    private fun uploadImageToFirebase(uri: Uri) {
+        val storageRef = FirebaseStorage.getInstance().reference
+        val imagesRef = storageRef.child("scooters/${name}.jpg")
+
+        //update the image in the database
+        RidesDB.updateScooterImage(name!!, "${name}.jpg")
+
+        // Get a scaled bitmap from the URI
+        val scaledBitmap = getScaledBitmap(uri.path!!, 640, 480)
+
+        // Compress the bitmap to a JPEG format
+        val baos = ByteArrayOutputStream()
+        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        // Upload the data to Firebase Storage
+        val uploadTask = imagesRef.putBytes(data)
+        uploadTask.addOnSuccessListener { taskSnapshot ->
+            imagesRef.downloadUrl.addOnSuccessListener { downloadUrl ->
+                // Update the scooter image URL in the Firebase Realtime Database
+                val scooterRef =
+                    FirebaseDatabase.getInstance().getReference("scooters").child(name!!)
+                scooterRef.child("image").setValue(downloadUrl.toString())
+                Log.d("Image", "Image uploaded to Firebase Storage and database updated")
+                Toast.makeText(requireContext(), "Image uploaded successfully", Toast.LENGTH_SHORT)
+                    .show()
+            }.addOnFailureListener { exception ->
+                Log.e("Image", "Failed to get download URL for image", exception)
+                Toast.makeText(requireContext(), "Failed to upload image", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }.addOnFailureListener { exception ->
+            Log.e("Image", "Failed to upload image to Firebase Storage", exception)
+            Toast.makeText(requireContext(), "Failed to upload image", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+
+
+    private fun openCamera() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val photoFile = File(photoName!!)
+            val photoUri = Uri.fromFile(photoFile)
+            val intent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, photoUri)
+            requireActivity().sendBroadcast(intent)
+            uploadImageToFirebase(photoUri)
+        } else {
+            requestCameraPermission()
+        }
+    }
+
+
+    private fun requestCameraPermission() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            Snackbar.make(
+                requireView(),
+                "Camera access is required to take pictures",
+                Snackbar.LENGTH_INDEFINITE
+            ).setAction("OK") {
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+            }.show()
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (photoName != null) {
+                    openCamera()
+                } else {
+                    Toast.makeText(requireContext(), "Failed to generate photo name", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(requireContext(), "Camera permission denied", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
 
     private fun stopUpdatingCounter() {
         isTextViewUpdating = false
